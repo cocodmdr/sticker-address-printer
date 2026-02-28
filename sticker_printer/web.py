fa17333251fbbf853cafb556abfbd7f6a15734b8
Corentin de Massol <cdemassol@lely.com>
2026-02-28 22:58:19 +0100

    fix: serve static CSS/images from project static directory

--- sticker_printer/web.py
+++ sticker_printer/web.py
@@ -63,7 +63,7 @@ def _extract_form_and_addresses(req):
 
 
 def create_app():
-    app = Flask(__name__, template_folder="../templates")
+    app = Flask(__name__, template_folder="../templates", static_folder="../static", static_url_path="/static")
 
     @app.get("/")
     def index():
